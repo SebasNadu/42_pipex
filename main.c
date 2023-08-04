4de77723d3e09f2b1d7c21a1bed7b5449f447034
Johan Sebastian Navarro Duarte <johnavar@c4a3c8.42berlin.de>
2023-08-04 11:37:47 +0200

    ex creating a pipe

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: johnavar <johnavar@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/28 13:05:13 by johnavar          #+#    #+#             */
-/*   Updated: 2023/07/28 14:30:15 by johnavar         ###   ########.fr       */
+/*   Updated: 2023/08/04 11:31:23 by johnavar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #include <stdio.h>
 #include <sys/wait.h>
 
-int	main(void)
+/*int	main(void)
 {
 	pid_t	id;
 	pid_t	id2;
@@ -52,5 +52,40 @@ int	main(void)
 	}
 	if (id != 0)
 		printf("\n");
+	return (0);
+}*/
+
+int	main(int ac, char **av)
+{
+	int	fd[2];
+	int	pid1;
+	int	pid2;
+
+	if (pipe(fd) == -1)
+		return (1);
+	pid1 = fork();
+	if (pid1 < 0)
+		return (2);
+	if (pid1 == 0)
+	{
+		dup2(fd[1], STDOUT_FILENO);
+		close(fd[0]);
+		close(fd[1]);
+		execlp("ping", "ping", "-c", "5", "google.com", NULL);
+	}
+	pid2 = fork();
+	if (pid2 < 0)
+		return (3);
+	if (pid2 == 0)
+	{
+		dup2(fd[0], STDIN_FILENO);
+		close(fd[0]);
+		close(fd[1]);
+		execlp("grep", "grep", "rtt", NULL);
+	}
+	close(fd[0]);
+	close(fd[1]);
+	waitpid(pid1, NULL, 0);
+	waitpid(pid2, NULL, 0);
 	return (0);
 }
