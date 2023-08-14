17b43065a5218b6aa383512badd66209395ba9b4
Johan Sebastian Navarro Duarte <johnavar@c4a3c8.42berlin.de>
2023-08-14 18:32:33 +0200

    some struct

--- main.c
+++ main.c
@@ -6,86 +6,38 @@
 /*   By: johnavar <johnavar@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/28 13:05:13 by johnavar          #+#    #+#             */
-/*   Updated: 2023/08/04 11:31:23 by johnavar         ###   ########.fr       */
+/*   Updated: 2023/08/14 17:15:55 by johnavar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
 #include <sys/wait.h>
+#include <fcntl.h>
 
-/*int	main(void)
+/* TO DO
+ * init struct
+ * checks args
+ * parse cmds
+ * parse args
+ * exec
+ * cleanup
+*/
+
+int	pipex(int f1, int f2, char **ag, char **envp)
 {
-	pid_t	id;
-	pid_t	id2;
-	int		n;
-	int		i;
-
-	id = fork();
-	if (id == 0)
-	{
-		printf("We are process y\n");
-		n = 1;
-	}
-	else
-	{
-		id2 = fork();
-		if (id2 == 0)
-		{
-			printf("We are process z\n");
-			n = 6;
-		}
-		else
-		{
-			printf("We are parent process\n");
-			n = 11;
-			wait(NULL);
-		}
-		wait(NULL);
-	}
-	i = n;
-	while (i < n + 5)
-	{
-		printf("%d,", i);
-		fflush(stdout);
-		i++;
-	}
-	if (id != 0)
-		printf("\n");
 	return (0);
-}*/
+}
 
-int	main(int ac, char **av)
+int	main(int ac, char **ag, char **envp)
 {
-	int	fd[2];
-	int	pid1;
-	int	pid2;
+	int	f1;
+	int	f2;
 
-	if (pipe(fd) == -1)
-		return (1);
-	pid1 = fork();
-	if (pid1 < 0)
-		return (2);
-	if (pid1 == 0)
-	{
-		dup2(fd[1], STDOUT_FILENO);
-		close(fd[0]);
-		close(fd[1]);
-		execlp("ping", "ping", "-c", "5", "google.com", NULL);
-	}
-	pid2 = fork();
-	if (pid2 < 0)
-		return (3);
-	if (pid2 == 0)
-	{
-		dup2(fd[0], STDIN_FILENO);
-		close(fd[0]);
-		close(fd[1]);
-		execlp("grep", "grep", "rtt", NULL);
-	}
-	close(fd[0]);
-	close(fd[1]);
-	waitpid(pid1, NULL, 0);
-	waitpid(pid2, NULL, 0);
+	f1 = open(ag[1], O_RDONLY);
+	f2 = open(ag[4], O_CREAT | O_RDWR | O_TRUNC, 0644);
+	if (f1 < 0 || f2 < 0)
+		return (-1);
+	pipex(f1, f2, ag, envp);
 	return (0);
 }
