322dd0c20d1ada545d678180bbd76c78069a5942
Sebastian Navarro <sebastiannavarro.fl@gmail.com>
2023-09-05 15:31:42 +0200

    add parse args and parse path

--- includes/pipex.h
+++ includes/pipex.h
@@ -6,7 +6,7 @@
 /*   By: sebasnadu <johnavar@student.42berlin.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/25 11:24:25 by sebasnadu         #+#    #+#             */
-/*   Updated: 2023/09/04 12:18:29 by sebasnadu        ###   ########.fr       */
+/*   Updated: 2023/09/05 12:37:31 by sebasnadu        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,13 @@
 # define PIPEX_H
 
 # include "../libft/includes/libft.h"
+# include "../libft/includes/ft_printf.h"
+
+#include <unistd.h>
+#include <fcntl.h>
+
 
 # define HERE_DOC_PATH ".pipex_here_doc"
-# define INVALID_INPUT_PATH ".pipex_error"
 # define URANDOM_PATH ".pipex_urandom"
 
 enum e_pipex_err
@@ -46,10 +50,12 @@ typedef struct s_pipex
 	int		fd_out;
 	t_bool	here_doc;
 	t_bool	is_urandom;
-	t_bool	is_invalid_input;
 	char	**cmd_paths;
 	char	***cmd_args;
 	int		cmd_count;
 }				t_pipex;
+
+void	*pipex_exit(t_pipex *pipex, char *param, int err);
+void	free_array(char **array, int size);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,92 +6,106 @@
 /*   By: johnavar <johnavar@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/28 13:05:13 by johnavar          #+#    #+#             */
-/*   Updated: 2023/09/04 12:34:56 by sebasnadu        ###   ########.fr       */
+/*   Updated: 2023/09/05 15:31:06 by sebasnadu        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pipex.h"
-#include <unistd.h>
 
-void	pipex_perror(char *param, int err)
+int	read_line(char **line, int fd, char delimiter)
 {
-	ft_putstr_fd("pipex: ", STDERR_FILENO);
-	if (err == CMD_NOT_FOUND)
-		ft_putstr_fd("command not found: ", STDERR_FILENO);
-	else if (err == NO_FILE)
-		ft_putstr_fd("no such file or directory: ", STDERR_FILENO);
-	else if (err == NO_AUTH)
-		ft_putstr_fd("permission denied: ", STDERR_FILENO);
-	else if (err == CMD_FAIL)
-		ft_putstr_fd("command failed: ", STDERR_FILENO);
-	else if (err == INV_ARGS)
-		ft_putstr_fd("invalid arguments: ", STDERR_FILENO);
-	else if (err == NO_MEMORY)
-		ft_putstr_fd("no memory left on device: ", STDERR_FILENO);
-	else if (err == DUP_ERR)
-		ft_putstr_fd("could not duplicate the fd: ", STDERR_FILENO);
-	else if (err == PIPE_ERR)
-		ft_putstr_fd("could not create the pipe: ", STDERR_FILENO);
-	else if (err == FORK_ERR)
-		ft_putstr_fd("could not create the child process: ", STDERR_FILENO);
-	if (param && (err == CMD_NOT_FOUND || err == NO_FILE || err == NO_AUTH ||
-		err == CMD_FAIL))
-		ft_putstr_fd(param, STDERR_FILENO);
-	ft_putstr_fd("\n", STDERR_FILENO);
+	char	*buffer;
+	char	c;
+	int		i;
 
+	i = 0;
+	if (fd < 0)
+		return (-1);
+	buffer = ft_calloc(10000, sizeof(char));
+	if (!buffer)
+		return (-1);
+	while (read(fd, &c, 1) > 0 && c && c != delimiter && i < 10000 - 2)
+		buffer[i++] = c;
+	buffer[i++] = '\n';
+	buffer[i] = '\0';
+	*line = buffer;
+	return (i);
 }
 
-void	free_array(char **array, int size)
+void	here_doc_handler(char *arg)
 {
-	int	i;
+	int		fd;
+	char	*buffer;
 
-	i = -1;
-	if (size == -1)
-		while (array[++i])
-			free(array[i]);
-	else
-		while (++i < size)
-			if (array[i])
-				free(array[i]);
-	free(array);
+	fd = open(HERE_DOC_PATH, O_RDWR | O_CREAT | O_TRUNC, 0644);
+	write(1, "heredoc> ", 9);
+	while (fd > 0 && read_line(&buffer, STDIN_FILENO, '\n') > 0)
+	{
+		if (ft_strncmp(buffer, arg, ft_strlen(arg)) == 0
+			&& ft_strlen(buffer) == ft_strlen(arg) + 1)
+		{
+			free(buffer);
+			break ;
+		}
+		write(fd, buffer, ft_strlen(buffer));
+		free(buffer);
+		write(1, "heredoc> ", 9);
+	}
+	close(fd);
 }
 
-void free_matrix(char ***matrix, int size)
+void	urandom_handler(void)
 {
-	int	i;
+	int		urandom_fd;
+	int		tmp_fd;
+	char	*buffer;
 
-	i = -1;
-	if (size == -1)
-		while (matrix[++i])
-			free_array(matrix[i], -1);
-	else
-		while (++i < size)
-			if (matrix[i])
-				free_array(matrix[i], -1);
-	free(matrix);
+	urandom_fd = open("/dev/urandom", O_RDONLY);
+	if (urandom_fd < 0)
+		return ;
+	tmp_fd = open(URANDOM_PATH, O_RDWR | O_CREAT | O_TRUNC, 0644);
+	if (tmp_fd < 0)
+		return ;
+	if (read_line(&buffer, urandom_fd, '\0') == -1)
+		return ;
+	write(tmp_fd, buffer, ft_strlen(buffer));
+	free(buffer);
+	close(tmp_fd);
 }
 
-void	*pipex_exit(t_pipex *pipex, char *param, int err)
+t_bool	get_infile(t_pipex *pipex, char **argv)
 {
-	if (err < 1 || param)
-		pipex_perror(param, err);
-	if (pipex->fd_in != -1)
-		close(pipex->fd_in);
-	if (pipex->fd_out != -1)
-		close(pipex->fd_out);
-	if (pipex->cmd_paths != NULL)
-		free_array(pipex->cmd_paths, pipex->cmd_count);
-	if (pipex->cmd_args != NULL)
-		free_matrix(pipex->cmd_args, pipex->cmd_count);
 	if (pipex->here_doc)
-		unlink(HERE_DOC_PATH);
-	if (pipex->is_urandom)
-		unlink(URANDOM_PATH);
-	if (pipex->is_invalid_input)
-		unlink(INVALID_INPUT_PATH);
-	free(pipex);
-	exit(0);
-	return (0);
+	{
+		here_doc_handler(argv[2]);
+		pipex->fd_in = open(HERE_DOC_PATH, O_RDONLY);
+	}
+	else if (pipex->is_urandom)
+	{
+		urandom_handler();
+		pipex->fd_in = open(URANDOM_PATH, O_RDONLY);
+	}
+	else
+	{
+		if (access(argv[1], F_OK) == -1)
+			return (false);
+		else
+			pipex->fd_in = open(argv[1], O_RDONLY);
+	}
+	if (pipex->fd_in < 0)
+		return (false);
+	return (true);
+}
+
+t_bool	get_outfile(t_pipex *pipex, char **argv, int argc)
+{
+	if (pipex->here_doc)
+		pipex->fd_out = open(argv[argc - 1], O_WRONLY | O_CREAT | O_APPEND, 0644);
+	else
+		pipex->fd_out = open(argv[argc - 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (pipex->fd_out < 0)
+		return (false);
+	return (true);
 }
 
 t_bool	parse_args(int argc, char **argv, t_pipex *pipex)
@@ -102,13 +116,80 @@ t_bool	parse_args(int argc, char **argv, t_pipex *pipex)
 		&& ft_strlen(argv[1]) == 12)
 		pipex->is_urandom = true;
 	if (argc < 5 + (int)pipex->here_doc)
-		return (false);
+		return (*(int *)pipex_exit(NULL, NULL, INV_ARGS));
 	if (!pipex->here_doc && access(argv[1], F_OK) == -1)
-		return (NO_FILE);
+		return (*(int *)pipex_exit(NULL, argv[1], NO_FILE));
 	if (!pipex->here_doc && access(argv[1], R_OK) == -1)
-		return (NO_AUTH);
-	if (parse_args(argc, argv, pipex))
-		return (INV_ARGS);
+		return (*(int *)pipex_exit(NULL, argv[1], NO_AUTH));
+	if (get_infile(pipex, argv) == false)
+		return (*(int *)pipex_exit(pipex, argv[1], NO_FILE));
+	if (get_outfile(pipex, argv, argc) == false)
+		return (*(int *)pipex_exit(pipex, argv[argc - 1], NO_FILE));
+	pipex->cmd_count = argc - 3 - (int)pipex->here_doc;
+	return (true);
+}
+
+char	**get_paths(char **envp, char *key)
+{
+	int		i;
+	char	*path;
+
+	i = -1;
+	while (envp[++i])
+		if (ft_strncmp(key, envp[i], ft_strlen(key)) == 0
+			&& envp[i][ft_strlen(key)] == '=')
+				path = ft_strchr(envp[i], '=') + 1;
+	if (!path)
+		return (NULL);
+	return (ft_split(path, ':'));
+}
+
+char	*find_cmd_path(char *cmd, char **envp)
+{
+	int		i;
+	char	**paths;
+	char	*path;
+	char	*full_path;
+
+	if (access(cmd, F_OK) == 0)
+		return (ft_substr(cmd, 0, ft_strlen(cmd)));
+	paths = get_paths(envp, "PATH");
+	if (!paths)
+		return (NULL);
+	i = -1;
+	while (paths[++i])
+	{
+		path = ft_strjoin(paths[i], "/");
+		if (!path)
+			return (free_array(paths, -1), NULL);
+		full_path = ft_strjoin(path, cmd);
+		free(path);
+		if (!full_path)
+			return (free_array(paths, -1), NULL);
+		if (access(full_path, F_OK) == 0)
+			return (free_array(paths, -1), full_path);
+		free(full_path);
+	}
+	return (free_array(paths, -1), NULL);
+}
+
+t_bool	parse_paths(t_pipex *pipex, int argc, char **argv, char **envp)
+{
+	int		i;
+	char	**cmd;
+
+	pipex->cmd_paths = ft_calloc(pipex->cmd_count + 1, sizeof(char *));
+	if (!pipex->cmd_paths)
+		return (false);
+	i = 1 + pipex->here_doc;
+	while (++i < argc - 1)
+	{
+		cmd = ft_split(argv[i], ' ');
+		if (!cmd)
+			return (false);
+		pipex->cmd_paths[i - 2 - pipex->here_doc] = find_cmd_path(cmd[0], envp);
+		free_array(cmd, -1);
+	}
 	return (true);
 }
 
@@ -118,7 +199,6 @@ void	init_pipex(t_pipex *pipex)
 	pipex->fd_out = -1;
 	pipex->here_doc = false;
 	pipex->is_urandom = false;
-	pipex->is_invalid_input = false;
 	pipex->cmd_paths = NULL;
 	pipex->cmd_args = NULL;
 	pipex->cmd_count = 0;
@@ -132,5 +212,9 @@ int	main(int argc, char **argv, char **envp)
 	if (!pipex)
 		return(*(int *)pipex_exit(NULL, NULL, NO_MEMORY));
 	init_pipex(pipex);
+	if (parse_args(argc, argv, pipex) == false)
+		return (1);
+	if (parse_paths(pipex, argc, argv, envp) == false)
+		return (*(int *)pipex_exit(pipex, NULL, NO_PATH));
 	return (*(int *)pipex_exit(pipex, NULL, END));
 }
